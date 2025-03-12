b19b447613b5fa848470c2cc93f1866873af7d3d
leandro jader <ldandro>
2025-03-12 14:47:19 -0300

    Add project files.

---
+++ jogo_da_forca.console.app/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace jogo_da_forca.console.app
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
