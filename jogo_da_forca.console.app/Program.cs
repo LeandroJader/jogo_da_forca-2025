d6e9ae2d8d279ee0a7c276431fc96c76614600e4
leandroJader <ldandro>
2025-03-18 10:26:49 -0300

    implementada refatoração de codigo

--- jogo_da_forca.console.app/Program.cs
+++ jogo_da_forca.console.app/Program.cs
@@ -4,128 +4,144 @@
     {
         static void Main(string[] args)
         {
-            string[] palavras = {
-                "ABACATE",
-                "ABACAXI",
-                "ACEROLA",
-                "ACAI",
-                "ARACA",
-                "ABACATE",
-                "BACABA",
-                "BACURI",
-                "BANANA",
-                "CAJA",
-                "CAJU",
-                "CARAMBOLA",
-                "CUPUACU",
-                "GRAVIOLA",
-                "GOIABA",
-                "JABUTICABA",
-                "JENIPAPO",
-                "MACA",
-                "MANGABA",
-                "MANGA",
-                "MARACUJA",
-                "MURICI",
-                "PEQUI",
-                "PITANGA",
-                "PITAYA",
-                "SAPOTI",
-                "TANGERINA",
-                "UMBU",
-                "UVA",
-                "UVAIA"
-            };
-
-            Random random = new Random();
-
-            int indiceEscolhido = random.Next(palavras.Length);
-
-            string palavraEscolhida = palavras[indiceEscolhido];
-
-            char[] letrasEncontradas = new char[palavraEscolhida.Length];
-
-            for (int caractere = 0; caractere < letrasEncontradas.Length; caractere++)
-                letrasEncontradas[caractere] = '_';
-
-            int quantidadeErros = 0;
-            bool jogadorEnforcou = false;
-            bool jogadorAcertou = false;
-
-            do
+            while (true)
             {
-                string cabecaDoBoneco = quantidadeErros >= 1 ? " o " : " ";
-                string tronco = quantidadeErros >= 2 ? "x" : " ";
-                string troncoBaixo = quantidadeErros >= 2 ? " x " : " ";
-                string bracoEsquerdo = quantidadeErros >= 3 ? "/" : " ";
-                string bracoDireito = quantidadeErros >= 4 ? @"\" : " ";
-                string pernas = quantidadeErros >= 5 ? "/ \\" : " ";
+                string[] palavras = MenuDePalavras();
+                string palavraEscolhida = EscolherPalavra(palavras);
+                char[] letrasEncontradas = InicializarLetrasEncontradas(palavraEscolhida);
+                int quantidadeErros = 0;
+                bool jogadorEnforcou = false;
+                bool jogadorAcertou = false;
 
-                Console.Clear();
-                Console.WriteLine("===============================================");
-                Console.ForegroundColor = ConsoleColor.Green;
-                Console.WriteLine("Jogo da Forca");
-                Console.ResetColor();
-                Console.WriteLine("===============================================");
-                Console.WriteLine(" ___________        ");
-                Console.WriteLine(" |/        |        ");
-                Console.WriteLine(" |        {0}       ", cabecaDoBoneco);
-                Console.WriteLine(" |        {0}{1}{2} ", bracoEsquerdo, tronco, bracoDireito);
-                Console.WriteLine(" |        {0}       ", troncoBaixo);
-                Console.WriteLine(" |        {0}       ", pernas);
-                Console.WriteLine(" |                  ");
-                Console.WriteLine(" |                  ");
-                Console.WriteLine("_|____              ");
-                Console.WriteLine("===============================================");
-                Console.WriteLine("Erros do jogador: " + quantidadeErros);
-                Console.WriteLine("===============================================");
-                Console.WriteLine("Palavra escolhida: " + String.Join("", letrasEncontradas));
-                Console.WriteLine("===============================================");
-
-                Console.Write("Digite uma letra: ");
-                char chute = Console.ReadLine()!.ToUpper()[0];
-
-                bool letraFoiEncontrada = false;
-
-                for (int contador = 0; contador < palavraEscolhida.Length; contador++)
+                do
                 {
-                    char letraAtual = palavraEscolhida[contador];
+                    ExibirForca(quantidadeErros);
+                    ExibirPalavra(letrasEncontradas);
+                    Console.Write("Digite uma letra: ");
+                    char chute = Char.ToUpper(Console.ReadLine()[0]);
 
-                    if (chute == letraAtual)
+                    bool letraFoiEncontrada = VerificarLetra(palavraEscolhida, chute, letrasEncontradas);
+                    if (!letraFoiEncontrada)
                     {
-                        letrasEncontradas[contador] = letraAtual;
-                        letraFoiEncontrada = true;
+                        quantidadeErros++;
                     }
-                }
 
-                if (letraFoiEncontrada == false)
-                    quantidadeErros++;
+                    jogadorAcertou = PalavraAcertada(palavraEscolhida, letrasEncontradas);
+                    jogadorEnforcou = quantidadeErros > 5;
 
-                string palavraEncontrada = String.Join("", letrasEncontradas);
+                    if (jogadorAcertou)
+                    {
+                        ExibirMensagemDeSucesso(palavraEscolhida);
+                    }
+                    else if (jogadorEnforcou)
+                    {
+                        ExibirMensagemDeFracasso(palavraEscolhida);
+                    }
 
-                jogadorAcertou = palavraEncontrada == palavraEscolhida;
-                jogadorEnforcou = quantidadeErros > 5;
+                    Console.ReadLine();
+                } while (!jogadorEnforcou && !jogadorAcertou);
+            }
+        }
 
-                if (jogadorAcertou)
+        static string[] MenuDePalavras()
+        {
+            return new string[]
+            {
+                "ABACATE", "ABACAXI", "ACEROLA", "ACAI", "ARACA", "BACABA", "BACURI",
+                "BANANA", "CAJA", "CAJU", "CARAMBOLA", "CUPUACU", "GRAVIOLA",
+                "GOIABA", "JABUTICABA", "JENIPAPO", "MACA", "MANGABA", "MANGA",
+                "MARACUJA", "MURICI", "PEQUI", "PITANGA", "PITAYA", "SAPOTI",
+                "TANGERINA", "UMBU", "UVA", "UVAIA"
+            };
+        }
+
+        static string EscolherPalavra(string[] palavras)
+        {
+            Random random = new Random();
+            int indiceEscolhido = random.Next(palavras.Length);
+            return palavras[indiceEscolhido];
+        }
+
+        static char[] InicializarLetrasEncontradas(string palavraEscolhida)
+        {
+            char[] letrasEncontradas = new char[palavraEscolhida.Length];
+            for (int i = 0; i < letrasEncontradas.Length; i++)
+            {
+                letrasEncontradas[i] = '_';
+            }
+            return letrasEncontradas;
+        }
+
+        static void ExibirForca(int quantidadeErros)
+        {
+            string cabecaDoBoneco = quantidadeErros >= 1 ? " o " : " ";
+            string tronco = quantidadeErros >= 2 ? "x" : " ";
+            string troncoBaixo = quantidadeErros >= 2 ? " x " : " ";
+            string bracoEsquerdo = quantidadeErros >= 3 ? "/" : " ";
+            string bracoDireito = quantidadeErros >= 4 ? @"\" : " ";
+            string pernas = quantidadeErros >= 5 ? "/ \\" : " ";
+
+            Console.Clear();
+            Console.WriteLine("===============================================");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Jogo da Forca");
+            Console.ResetColor();
+            Console.WriteLine("===============================================");
+            Console.WriteLine(" ___________        ");
+            Console.WriteLine(" |/        |        ");
+            Console.WriteLine(" |        {0}       ", cabecaDoBoneco);
+            Console.WriteLine(" |        {0}{1}{2} ", bracoEsquerdo, tronco, bracoDireito);
+            Console.WriteLine(" |        {0}       ", troncoBaixo);
+            Console.WriteLine(" |        {0}       ", pernas);
+            Console.WriteLine(" |                  ");
+            Console.WriteLine(" |                  ");
+            Console.WriteLine("_|____              ");
+            Console.WriteLine("===============================================");
+            Console.WriteLine("Erros do jogador: " + quantidadeErros);
+            Console.WriteLine("===============================================");
+        }
+
+        static void ExibirPalavra(char[] letrasEncontradas)
+        {
+            Console.WriteLine("Palavra escolhida: " + string.Join("", letrasEncontradas));
+            Console.WriteLine("===============================================");
+        }
+
+        static bool VerificarLetra(string palavraEscolhida, char chute, char[] letrasEncontradas)
+        {
+            bool letraFoiEncontrada = false;
+            for (int i = 0; i < palavraEscolhida.Length; i++)
+            {
+                if (chute == palavraEscolhida[i])
                 {
-                    Console.WriteLine("===============================================");
-                    Console.ForegroundColor = ConsoleColor.Green;
-                    Console.WriteLine($"Você acertou a palavra secreta {palavraEscolhida}, parabéns!");
-                    Console.ResetColor();
-                    Console.WriteLine("===============================================");
+                    letrasEncontradas[i] = chute;
+                    letraFoiEncontrada = true;
                 }
-                else if (jogadorEnforcou)
-                {
-                    Console.WriteLine("===============================================");
-                    Console.ForegroundColor = ConsoleColor.Red;
-                    Console.WriteLine("Que azar! Tente novamente!");
-                    Console.ResetColor();
-                    Console.WriteLine("===============================================");
-                }
+            }
+            return letraFoiEncontrada;
+        }
 
-                Console.ReadLine();
+        static bool PalavraAcertada(string palavraEscolhida, char[] letrasEncontradas)
+        {
+            return string.Join("", letrasEncontradas) == palavraEscolhida;
+        }
 
-            } while (jogadorEnforcou == false && jogadorAcertou == false);
+        static void ExibirMensagemDeSucesso(string palavraEscolhida)
+        {
+            Console.WriteLine("===============================================");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine($"Você acertou a palavra secreta {palavraEscolhida}, parabéns!");
+            Console.ResetColor();
+            Console.WriteLine("===============================================");
+        }
+
+        static void ExibirMensagemDeFracasso(string palavraEscolhida)
+        {
+            Console.WriteLine("===============================================");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine($"Que azar! A palavra secreta era {palavraEscolhida}. Tente novamente!");
+            Console.ResetColor();
+            Console.WriteLine("===============================================");
         }
     }
 }
